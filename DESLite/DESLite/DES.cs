64075fa3099c1119648647ec9d78c064260f4cf1
VisualStudioVPC <ander_n@ooutliik.com>
2016-08-27 09:49:01 +0800

    简单DES加密算法

---
+++ DESLite/DESLite/DES.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DESLite
+{
+    public class DES
+    {
+        private static byte[] Keys = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+        private static string codeKey;
+
+        public string DESLite(Boolean isSec,string str, string secKey)
+        {
+            codeKey = secKey; //8位密钥
+            if (isSec)
+            {
+                return EncryptDES(str);
+            }
+            else
+            {
+                return DecryptDES(str);
+            }
+        }       
+
+        //DES加密解密
+       // private static byte[] Keys = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+       // private static string codeKey = "fjkdashg"; //8位密钥
+
+        //加密
+        public static string EncryptDES(string encryptString)
+        {
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(codeKey.Substring(0, 8));
+                byte[] rgbIV = Keys;
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(encryptString);
+                DESCryptoServiceProvider dCSP = new DESCryptoServiceProvider();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, dCSP.CreateEncryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Convert.ToBase64String(mStream.ToArray());
+            }
+            catch
+            {
+                return "保存错误" + encryptString;
+            }
+        }
+
+        //解密
+        public static string DecryptDES(string decryptString)
+        {
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(codeKey);
+                byte[] rgbIV = Keys;
+                byte[] inputByteArray = Convert.FromBase64String(decryptString);
+                DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Encoding.UTF8.GetString(mStream.ToArray());
+            }
+            catch
+            {
+                return "读取失败" + decryptString;
+            }
+        }
+    }
+}
