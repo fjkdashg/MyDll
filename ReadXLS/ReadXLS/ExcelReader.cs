2df6d3b433ea98f230313be4b75ce45dfe666d59
VisualStudioVPC <ander_n@ooutliik.com>
2016-09-01 09:51:44 +0800

    建立读Excel库

---
+++ ReadXLS/ReadXLS/ExcelReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ReadXLS
+{
+    public class ExcelReader
+    {
+        //读取Excel到DataSe
+            public DataSet ExcelToDS(string Path)
+        {
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;";
+            OleDbConnection conn = new OleDbConnection(strConn);
+
+            //获取Excel Sheet表名称
+            DataTable schemaTable = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, null);
+            string tableName = schemaTable.Rows[0][2].ToString().Trim();
+
+            //读取数据
+            conn.Open();
+            string strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataSet ds = null;
+            strExcel = "select * from ["+ tableName + "]";
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+            return ds;
+        }
+    }
+}
