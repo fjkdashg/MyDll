311a0954a67c3e8729357612f858aa9de76202dc
VisualStudioVPC <ander_n@ooutliik.com>
2016-09-13 09:43:12 +0800

    日志

--- RunLog/RunLog/LogClass.cs
+++ RunLog/RunLog/LogClass.cs
@@ -35,7 +35,7 @@ namespace RunLog
         {
             
                 string ProPath = System.Environment.CurrentDirectory;
-                FileStream fs = new FileStream(@ProPath + "\\Log\\" + DateTime.Now.ToLongDateString().ToString() + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+                FileStream fs = new FileStream(@ProPath + "\\Log_" + DateTime.Now.ToLongDateString().ToString() + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
                 StreamWriter sw = new StreamWriter(fs);
                 sw.BaseStream.Seek(0, SeekOrigin.End);
                 sw.WriteLine(DateTime.Now.ToString() + LogLine + "\n");
