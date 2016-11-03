0efd8b9d0b4ae3e2305b2c5f2a817e82603522e0
ander_n@ComVPC <ander_n@outlook.com>
2016-11-03 16:54:19 +0800

    新增xlsx读取方式

--- ReadXLS/ReadXLS/ExcelReader.cs
+++ ReadXLS/ReadXLS/ExcelReader.cs
@@ -10,34 +10,10 @@ namespace ReadXLS
 {
     public class ExcelReader
     {
-        //读取Excel到DataSet
-            public DataSet ExcelToDS(string Path)
-        {
-            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;";
-            OleDbConnection conn = new OleDbConnection(strConn);           
-
-            //读取数据
-            conn.Open();
-
-            //获取Excel Sheet表名称
-            DataTable schemaTable = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, null);
-            string tableName = schemaTable.Rows[0][2].ToString().Trim();
-
-            string strExcel = "";
-            OleDbDataAdapter myCommand = null;
-            DataSet ds = null;
-            strExcel = "select * from ["+ tableName + "]";
-            myCommand = new OleDbDataAdapter(strExcel, strConn);
-            ds = new DataSet();
-            myCommand.Fill(ds, "table1");
-            return ds;
-        }
-
-
         //选择读取Excel到DataSet
         public DataSet ExcelListToDS(string Path,string DataList)
         {
-            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;";
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;HDR=YES;IMEX=2";
             OleDbConnection conn = new OleDbConnection(strConn);
 
             //读取数据
@@ -55,6 +31,29 @@ namespace ReadXLS
             ds = new DataSet();
             myCommand.Fill(ds, "table1");
             return ds;
+        }
+
+        //选择读取Excel到DataTable
+        public DataTable ExcelXLSXToDT(string Path, string DataList)
+        {
+            string strConn = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 12.0;HDR=YES;IMEX=2";
+            OleDbConnection conn = new OleDbConnection(strConn);
+
+            //读取数据
+            conn.Open();
+
+            //获取Excel Sheet表名称
+            DataTable schemaTable = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, null);
+            string tableName = schemaTable.Rows[0][2].ToString().Trim();
+
+            string strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataSet ds = null;
+            strExcel = "select " + DataList + " from [" + tableName + "]";
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+            return ds.Tables[0];
         }
 
     }
