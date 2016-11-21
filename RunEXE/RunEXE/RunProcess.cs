9046d189fa41eb10c4122aa90bc5a5b340466e34
Ander <Ander@HiServerVPC>
2016-11-21 16:43:45 +0800

    RunEXE添加锁屏代码函数

--- RunEXE/RunEXE/RunProcess.cs
+++ RunEXE/RunEXE/RunProcess.cs
@@ -23,6 +23,20 @@ namespace RunEXE
             Proc = System.Diagnostics.Process.Start(p);//调用外部程序
         }
 
+        public void LockScreen()
+        {
+            Process Proc;
+            ProcessStartInfo p = new ProcessStartInfo("rundll32.exe", "user32.dll,LockWorkStation");
+            /*
+            p.WorkingDirectory = exepath;//设置此外部程序所在windows目录
+            p.WindowStyle = ProcessWindowStyle.Hidden;
+            //在调用外部exe程序的时候，控制台窗口不弹出
+            //如果想获得当前路径为
+            //string path = System.AppDomain.CurrentDomain.BaseDirectory;
+            */
+            Proc = System.Diagnostics.Process.Start(p);//调用外部程序
+        }
+
         public void ShowWebPage(string webURL)
         {
             Process Proc;
