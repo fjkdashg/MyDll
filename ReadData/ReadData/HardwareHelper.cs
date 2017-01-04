a2075eb8b5a18cf501cd90974b032faa24ad68ba
AnderNotting <Ander@Surface>
2017-01-04 14:30:09 +0800

    修正程序名称为 data

--- ReadData/ReadData/HardwareHelper.cs
+++ ReadData/ReadData/HardwareHelper.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Management;
 
-namespace ReadDate
+namespace ReadData
 {
     public class HardwareHelper
     {
--- ReadData/ReadData/LicenseHelper.cs
+++ ReadData/ReadData/LicenseHelper.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReadDate
+namespace ReadData
 {
     public class LicenseHelper
     {
