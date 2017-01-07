2dc4b5a456923f9ae74848d6c8a5aa62a4b498fb
AnderNotting <Ander@Surface>
2017-01-07 17:50:20 +0800

    FH:添加saveHttpFileWork事件，下载网络数据。未测试

--- ReadData/ReadData/FileHelper.cs
+++ ReadData/ReadData/FileHelper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,9 @@ namespace ReadData
 {
     public  class FileHelper
     {
+        public BackgroundWorker SaveHttpFileWork = new BackgroundWorker();
+
+
         public int processPer = 0;
         //输出内嵌资源文件
         public string SaveResourceFile(string FileSavePath, Stream SouFS)
@@ -19,7 +24,14 @@ namespace ReadData
             //Assembly app = Assembly.GetExecutingAssembly();
             //Stream SouFS = app.GetManifestResourceStream(ResFile);
 
+            //判断文件夹是否存在
+            if (!Directory.Exists(Path.GetDirectoryName(FileSavePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(FileSavePath));
+            }
+
             BinaryReader BR = new BinaryReader(SouFS);
+            Console.WriteLine(@FileSavePath);
             FileStream FS = new FileStream(@FileSavePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
             BinaryWriter BW = new BinaryWriter(FS);
 
@@ -36,14 +48,52 @@ namespace ReadData
             BW.Close();
             FS.Close();
             SouFS.Close();
-            return "OK,"+ @FileSavePath;
+            return @FileSavePath;
         }
 
         //输出网络文件
-        public string SaveHttpFile(string URL,string SavePath)
+        public void SaveHttpFileWork_Initial()
         {
+            SaveHttpFileWork.WorkerReportsProgress = true;  //允许报告进度
+            SaveHttpFileWork.WorkerSupportsCancellation = true;
+            SaveHttpFileWork.DoWork += new DoWorkEventHandler(SaveHttpFileWork_DoWork);  //产生新的线程来处理任务
+            //SaveHttpFileWork.ProgressChanged += new ProgressChangedEventHandler(BGWDownloading_ProgressChanged);  //当调用ReportProgress会触发该事件
+            //SaveHttpFileWork.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BGWDownloading_RunWorkerCompleted);
+        }
 
-            return "SaveFullPath";
+        public void SaveHttpFileWork_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] AppInfo = e.Argument as string[];
+            string FileURL = AppInfo[0];
+            string FilePath =AppInfo[1];
+
+            if (!Directory.Exists(Path.GetDirectoryName(FilePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(FilePath));
+            }
+
+            HttpWebRequest request = WebRequest.Create(FileURL) as HttpWebRequest;
+            //发送请求并获取相应回应数据
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求
+            long fullsize = response.ContentLength;
+            Stream responseStream = response.GetResponseStream();
+            //创建本地文件写入流
+            Stream stream = new FileStream(FilePath, FileMode.Create);
+            //stream.Length;
+            byte[] bArr = new byte[1024];
+            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            long loadSize = 0;
+            while (size > 0)
+            {
+                stream.Write(bArr, 0, size);
+                size = responseStream.Read(bArr, 0, (int)bArr.Length);
+                loadSize += size;
+                SaveHttpFileWork.ReportProgress((int)(loadSize * 100 / fullsize));
+            }
+            stream.Close();
+            responseStream.Close();
+            e.Result = FilePath;
         }
     }
 }
